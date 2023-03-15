1922ff72952c5f71ee89dc5de6c9c25675ef90cd
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-03-15 12:00:39 +0100

    setup mantine in project (tweak official template)

---
+++ src/DemoComponent/DemoComponent.jsx
@@ -0,0 +1,11 @@
+import { Stack, Text } from "@mantine/core";
+
+export function DemoComponent() {
+  return (
+    <Stack align="center" mt={50}>
+      <Text size="xl" weight={500}>
+        Welcome to QuizIt!
+      </Text>
+    </Stack>
+  );
+}
