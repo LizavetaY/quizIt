9b124b2aefdfd010f5da8a459a95565f3d6979eb
KatePrakofyeva <katajina@icloud.com>
2023-03-16 13:45:55 +0100

    fix: fixed mistakes

--- src/data/fakeData.js
+++ src/data/fakeData.js
@@ -1,34 +1,45 @@
 const reactRules = [
   {
     id: 1,
-    question: "What is a hook?",
+    question: "What is a Hook?",
     answers: [
-      "a JavaScript library for building user interfaces",
-      "a javascript function, allows you to create/access state and life cycles",
-      "one of the core building blocks of React",
-      "the standard routing library for React",
+      {
+        id: "ans-1-1",
+        data: "A JavaScript library for building user interfaces",
+      },
+      {
+        id: "ans-1-2",
+        data: "A javascript function, allows you to create/access state and life cycles",
+      },
+      { id: "ans-1-3", data: "One of the core building blocks of React" },
+      { id: "ans-1-4", data: "The standard routing library for React" },
     ],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-1-2",
+    explanation: "Explanation will be here",
   },
   {
     id: 2,
     question: "When Hooks were launched in React?",
     answers: [
-      "in React 16.7.0",
-      "first released in October of 2022",
-      "in React 16.8",
-      "29 May 2013",
+      { id: "ans-2-1", data: "In React 16.7.0" },
+      { id: "ans-2-2", data: "First released in October of 2022" },
+      { id: "ans-2-3", data: "In React 16.8" },
+      { id: "ans-2-4", data: "29 May 2013" },
     ],
-    isCorrect: answers[2],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-2-3",
+    explanation: "Explanation will be here",
   },
   {
     id: 3,
     question: "What plugin enforces two rules of Hooks?",
-    answers: ["TSLint", "ESLint", "Betterer", "Prettier"],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    answers: [
+      { id: "ans-3-1", data: "TSLint" },
+      { id: "ans-3-2", data: "ESLint" },
+      { id: "ans-3-3", data: "Betterer" },
+      { id: "ans-3-4", data: "Prettier" },
+    ],
+    correctAnswerId: "ans-3-2",
+    explanation: "Explanation will be here", // (if we wanna display it)
   },
 ];
 
@@ -36,39 +47,64 @@ const html = [
   {
     id: 1,
     question: "Which tag is used inside the <map> tag",
-    answers: ["<a>", "<map>", "<area>", "<img>"],
-    isCorrect: answers[2],
-    explanation: string, // (if we wanna display it)
+    answers: [
+      { id: "ans-1-1", data: "<a>" },
+      { id: "ans-1-2", data: "<map>" },
+      { id: "ans-1-3", data: "<area>" },
+      { id: "ans-1-4", data: "<img>" },
+    ],
+    correctAnswerId: "ans-1-3",
+    explanation: "Explanation will be here",
   },
   {
     id: 2,
     question:
       "What value of the input element's type attribute does not exist?",
-    answers: ["date", "month", "week", "day"],
-    isCorrect: answers[3],
-    explanation: string, // (if we wanna display it)
+    answers: [
+      { id: "ans-2-1", data: "Date" },
+      { id: "ans-2-2", data: "Month" },
+      { id: "ans-2-3", data: "Week" },
+      { id: "ans-2-4", data: "Day" },
+    ],
+    correctAnswerId: "ans-2-4",
+    explanation: "Explanation will be here",
   },
   {
     id: 3,
     question: "Which of the tags does NOT have the autocomplete attribute?",
-    answers: ["applies to all", "textarea", "form", "input"],
-    isCorrect: answers[0],
-    explanation: string, // (if we wanna display it)
+    answers: [
+      { id: "ans-3-1", data: "Applies to all" },
+      { id: "ans-3-2", data: "Textarea" },
+      { id: "ans-3-3", data: "Form" },
+      { id: "ans-3-4", data: "Input" },
+    ],
+    correctAnswerId: "ans-3-1",
+    explanation: "Explanation will be here",
   },
   {
     id: 4,
     question: "What value of type attribute does not exist for inputs?",
-    answers: ["number", "email", "color", "phone"],
-    isCorrect: answers[3],
-    explanation: string, // (if we wanna display it)
+    answers: [
+      { id: "ans-4-1", data: "Number" },
+      { id: "ans-4-2", data: "Email" },
+      { id: "ans-4-3", data: "Color" },
+      { id: "ans-4-4", data: "Phone" },
+    ],
+    correctAnswerId: "ans-4-4",
+    explanation: "Explanation will be here",
   },
   {
     id: 5,
     question:
       "What value should be given to the input tag's type attribute to create a radio button?",
-    answers: ["range", "radio", "button", "checkbox"],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    answers: [
+      { id: "ans-5-1", data: "range" },
+      { id: "ans-5-2", data: "radio" },
+      { id: "ans-5-3", data: "button" },
+      { id: "ans-5-4", data: "checkbox" },
+    ],
+    correctAnswerId: "ans-5-2",
+    explanation: "Explanation will be here",
   },
 ];
 
@@ -77,114 +113,123 @@ const css = [
     id: 1,
     question: "What types of positioning do you know?",
     answers: [
-      "static, sticky, absolute, relationship, fixed",
-      "static, stinky, absolute, relative, fixed",
-      "static, sticky, absolute, relative, fixed",
-      "flex, relative, absolutly",
+      { id: "ans-2-1", data: "Static, sticky, absolute, relationship, fixed" },
+      { id: "ans-2-2", data: "Static, stinky, absolute, relative, fixed" },
+      { id: "ans-2-3", data: "Static, sticky, absolute, relative, fixed" },
+      { id: "ans-2-4", data: "Flex, relative, absolutly" },
     ],
-    isCorrect: answers[2],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-2-3",
+    explanation: "Explanation will be here",
   },
   {
     id: 2,
     question: "What does CSS stand for?",
     answers: [
-      "Computer Style Sheets",
-      "Creative Style Sheets",
-      "Colorful Style Sheets",
-      "Cascading Style Sheets",
+      { id: "ans-2-1", data: "Computer Style Sheets" },
+      { id: "ans-2-2", data: "Creative Style Sheets" },
+      { id: "ans-2-3", data: "Colorful Style Sheets" },
+      { id: "ans-2-4", data: "Cascading Style Sheets" },
     ],
-    isCorrect: answers[3],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-2-4",
+    explanation: "Explanation will be here",
   },
   {
     id: 3,
     question: "What rule allows you to include external fonts?",
-    answers: ["@font-rule", "@font", "@font-face", "@fonts"],
-    isCorrect: answers[2],
-    explanation: string, // (if we wanna display it)
+    answers: [
+      { id: "ans-3-1", data: "@font-rule" },
+      { id: "ans-3-2", data: "@font" },
+      { id: "ans-3-3", data: "@font-face" },
+      { id: "ans-3-4", data: "@fonts" },
+    ],
+    correctAnswerId: "ans-3-3",
+    explanation: "Explanation will be here",
   },
   {
     id: 4,
     question: "What is the rgba() function responsible for?",
     answers: [
-      "color + transparency",
-      "color + saturation",
-      "color + contrast",
-      "color mixing",
+      { id: "ans-4-1", data: "Color + transparency" },
+      { id: "ans-4-2", data: "Color + saturation" },
+      { id: "ans-4-3", data: "Color + contrast" },
+      { id: "ans-4-4", data: "Color mixing" },
     ],
-    isCorrect: answers[0],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-4-1",
+    explanation: "Explanation will be here",
   },
   {
     id: 5,
     question: "What defines selector weight (specificity) in CSS?",
     answers: [
-      "number of selectors in code",
-      "style priority",
-      "how long will the page load",
-      "way to connect to HTML",
+      { id: "ans-5-1", data: "Number of selectors in code" },
+      { id: "ans-5-2", data: "Style priority" },
+      { id: "ans-5-3", data: "How long will the page load" },
+      { id: "ans-5-4", data: "Way to connect to HTML" },
     ],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-5-2",
+    explanation: "Explanation will be here",
   },
   {
     id: 6,
     question: "Which HTML attribute is used to define inline styles?",
     answers: [
-      "font",
-      "style",
-      "styles",
-      "class",
+      { id: "ans-6-1", data: "Font" },
+      { id: "ans-6-2", data: "Style" },
+      { id: "ans-6-3", data: "Styles" },
+      { id: "ans-6-4", data: "Class" },
     ],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-6-2",
+    explanation: "Explanation will be here",
   },
   {
     id: 7,
     question: "Which is the correct CSS syntax?",
     answers: [
-      "body {color: black;}",
-      "{body;color:black;}",
-      "{body:color=black;}",
-      "body:color=black;",
+      { id: "ans-7-1", data: "body {color: black;}" },
+      { id: "ans-7-2", data: "{body;color:black;}" },
+      { id: "ans-7-3", data: "{body:color=black;}" },
+      { id: "ans-7-4", data: "body:color=black;" },
     ],
-    isCorrect: answers[0],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-7-1",
+    explanation: "Explanation will be here",
   },
   {
     id: 8,
-    question: "What is the correct CSS syntax for making all the <p> elements bold?",
-    answers: ["<p style='font-size:bold;'>", 
-    "p {font-weight:bold;}",
-    "<p style='text-size:bold;'>", 
-    "p {text-size:bold;}"],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    question:
+      "What is the correct CSS syntax for making all the <p> elements bold?",
+    answers: [
+      { id: "ans-8-1", data: "<p style='font-size:bold;'>" },
+      { id: "ans-8-2", data: "p {font-weight:bold;}" },
+      { id: "ans-8-3", data: "<p style='text-size:bold;'>" },
+      { id: "ans-8-4", data: "p {text-size:bold;}" },
+    ],
+    correctAnswerId: "ans-8-2",
+    explanation: "Explanation will be here",
   },
   {
     id: 9,
-    question: "How do you make each word in a text start with a capital letter?",
+    question:
+      "How do you make each word in a text start with a capital letter?",
     answers: [
-      "text-transform:capitalize",
-      "text-style:capitalize",
-      "You can't do that with CSS",
-      "transform:capitalize",
+      { id: "ans-9-1", data: "text-transform:capitalize" },
+      { id: "ans-9-2", data: "text-style:capitalize" },
+      { id: "ans-9-3", data: "You can't do that with CSS" },
+      { id: "ans-9-4", data: "transform:capitalize" },
     ],
-    isCorrect: answers[0],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-9-1",
+    explanation: "Explanation will be here",
   },
   {
     id: 10,
     question: "How do you select an element with id 'demo'?",
     answers: [
-      "#demo",
-      "demo",
-      "*demo",
-      ".demo",
+      { id: "ans-10-1", data: "#demo" },
+      { id: "ans-10-2", data: "demo" },
+      { id: "ans-10-3", data: "*demo" },
+      { id: "ans-10-4", data: ".demo" },
     ],
-    isCorrect: answers[0],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-10-1",
+    explanation: "Explanation will be here",
   },
 ];
 
@@ -192,92 +237,101 @@ const js = [
   {
     id: 1,
     question: "What type of data is not in JS?",
-    answers: ["Symbol", "Char"],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    answers: [
+      { id: "ans-1-1", data: "Symbol" },
+      { id: "ans-1-2", data: "Char" },
+    ],
+    correctAnswerId: "ans-1-2",
+    explanation: "Explanation will be here",
   },
   {
     id: 2,
     question: "What is used to iterate over objects?",
-    answers: ["Object.keys", "for of"],
-    isCorrect: answers[0],
-    explanation: string, // (if we wanna display it)
+    answers: [
+      { id: "ans-2-1", data: "Object.keys" },
+      { id: "ans-2-2", data: "for of" },
+    ],
+    correctAnswerId: "ans-2-1",
+    explanation: "Explanation will be here",
   },
   {
     id: 3,
-    question: "What will be the result var foo = 10 + "20" ?",
-    answers: ["30", "'1020'"],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    question: "What will be the result: var foo = 10 + '20' ?",
+    answers: [
+      { id: "ans-3-1", data: "30" },
+      { id: "ans-3-2", data: "'1020'" },
+    ],
+    correctAnswerId: "ans-3-2",
+    explanation: "Explanation will be here",
   },
   {
     id: 4,
-    question: "'Use strict mode' helps in writing cleaner and safer code?",
+    question: "'Use strict' mode helps in writing cleaner and safer code?",
     answers: [
-      "True",
-      "False"
+      { id: "ans-4-1", data: "True" },
+      { id: "ans-4-2", data: "False" },
     ],
-    isCorrect: answers[0],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-4-1",
+    explanation: "Explanation will be here",
   },
   {
     id: 5,
-    question: "What will be the result let a = (1,5 - 1) * 2 ?",
+    question: "What will be the result: let a = (1,5 - 1) * 2 ?",
     answers: [
-      "1",
-      "8",
+      { id: "ans-5-1", data: "1" },
+      { id: "ans-5-2", data: "8" },
     ],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-5-2",
+    explanation: "Explanation will be here",
   },
   {
     id: 6,
     question: "null === undefined?",
     answers: [
-      "True",
-      "False"
+      { id: "ans-6-1", data: "True" },
+      { id: "ans-6-2", data: "False" },
     ],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-6-2",
+    explanation: "Explanation will be here",
   },
   {
     id: 7,
-    question: "let value = true; alert(typeof value)?",
+    question: "What will be the result: let value = true; alert(typeof value)?",
     answers: [
-      "boolean",
-      "undefined",
+      { id: "ans-7-1", data: "boolean" },
+      { id: "ans-7-2", data: "undefined" },
     ],
-    isCorrect: answers[0],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-7-1",
+    explanation: "Explanation will be here",
   },
   {
     id: 8,
     question: "Boolean any of these values 0, null, undefined, NaN, '' will?",
     answers: [
-      "True",
-      "False"
+      { id: "ans-8-1", data: "True" },
+      { id: "ans-8-2", data: "False" },
     ],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-8-2",
+    explanation: "Explanation will be here",
   },
   {
     id: 9,
     question: "What will be the result console.log( +true ) ?",
     answers: [
-      "1",
-      "True"
+      { id: "ans-9-1", data: "1" },
+      { id: "ans-9-2", data: "True" },
     ],
-    isCorrect: answers[0],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-9-1",
+    explanation: "Explanation will be here",
   },
   {
     id: 10,
     question: "What will be the result: let counter = 0; alert( counter++ ); ?",
     answers: [
-      "1",
-      "0"
+      { id: "ans-10-1", data: "1" },
+      { id: "ans-10-2", data: "0" },
     ],
-    isCorrect: answers[1],
-    explanation: string, // (if we wanna display it)
+    correctAnswerId: "ans-10-2",
+    explanation: "Explanation will be here",
   },
 ];
