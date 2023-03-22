e8097ec1f6f00d66ff05139bdf44754031ea6303
Lizaveta <liz07bokan@gmail.com>
2023-03-22 18:18:19 +0100

    feat: add progress bar

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { ColorSchemeToggle } from "./ColorSchemeToggle";
 export { Footer } from "./Footer";
 export { Header } from "./Header";
 export { Navbar } from "./Navbar";
+export { ProgressBar } from "./ProgressBar";
 export { QuizQuestion } from "./QuizQuestion";
