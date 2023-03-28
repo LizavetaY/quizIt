4cc61ae66e71d7114c0c2900d984862bd84bab82
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-03-28 11:50:41 +0200

    feat: add styles (WIP)

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,11 +1,55 @@
-import { Container, Text } from "@mantine/core";
+import { Link, useParams } from "react-router-dom";
+import {
+  Button,
+  Center,
+  Container,
+  Stack,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
 
 export const Results = ({ correctAnswersQty, quiz }) => {
+  const { colors } = useMantineTheme();
+  const { id: quizId } = useParams();
+
+  const totalQuestions = quiz.quizData.length;
+
   return (
-    <Container size="xs" px={0}>
-      <Text ta="center">
-        YOUR RESULT IS: {correctAnswersQty} / {quiz.quizData.length}
-      </Text>
+    <Container h="inherit" size="xs" px={0}>
+      <Center h="inherit">
+        <Stack align="center">
+          <Container
+            h={200}
+            w={200}
+            sx={{
+              borderRadius: "20px",
+              backgroundColor: colors.bgSecondary,
+            }}
+          >
+            <Center h="inherit">
+              <Stack>
+                <Text ta="center">
+                  YOUR RESULT: {correctAnswersQty} / {totalQuestions}
+                </Text>
+                <Text ta="center">100%</Text>
+              </Stack>
+            </Center>
+          </Container>
+          <Stack>
+            <Button
+              variant="primary"
+              radius="md"
+              component={Link}
+              to={`/${quizId}`}
+            >
+              Retry
+            </Button>
+            <Button component={Link} to={"/"} variant="secondary" radius="md">
+              To Home Page
+            </Button>
+          </Stack>
+        </Stack>
+      </Center>
     </Container>
   );
 };
