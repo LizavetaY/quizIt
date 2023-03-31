2da42d763daee0fa93cefc78dee36c3c2d839fd7
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-03-31 11:13:45 +0200

    refactor: add our GitHub links to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,52 @@
-import { Footer as MantineFooter, Text } from "@mantine/core";
+import { Flex, Footer as MantineFooter, Text } from "@mantine/core";
 
 export const Footer = () => {
   return (
     <MantineFooter>
-      <Text ta="center">Build with love by Liza, Kate and Mick 🙂 2023</Text>
+      <Flex justify="center" align="center" h="100%">
+        <Text ta="center">
+          Build with love by&nbsp;
+          <Text
+            component="a"
+            href="https://github.com/LizavetaY"
+            target="_blank"
+            sx={{
+              "&:hover": {
+                textDecoration: "underline",
+              },
+            }}
+          >
+            Liza
+          </Text>
+          &#44;&nbsp;
+          <Text
+            component="a"
+            href="https://github.com/HarryPotter-4000"
+            target="_blank"
+            sx={{
+              "&:hover": {
+                textDecoration: "underline",
+              },
+            }}
+          >
+            Kate
+          </Text>
+          &nbsp;and&nbsp;
+          <Text
+            component="a"
+            href="https://github.com/lazzzaroni"
+            target="_blank"
+            sx={{
+              "&:hover": {
+                textDecoration: "underline",
+              },
+            }}
+          >
+            Mick
+          </Text>
+          &nbsp;🙂 2023
+        </Text>
+      </Flex>
     </MantineFooter>
   );
 };
